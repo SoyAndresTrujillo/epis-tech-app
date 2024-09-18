ae405e8f89aaf20b48ea8327b9966716359301e3
soyandrestrujillo <andymania.1301@gmail.com>
2024-09-17 19:51:38 -0500

    feat(users): put users in users table in DB

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -7,26 +7,32 @@ const client = await db.connect();
 async function seedUsers() {
   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await client.sql`
-    CREATE TABLE IF NOT EXISTS users (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      full_name VARCHAR(255) NOT NULL,
-      email TEXT NOT NULL UNIQUE,
-      password TEXT NOT NULL
-      faculty TEXT NOT NULL
-      program TEXT NOT NULL
+  CREATE TABLE IF NOT EXISTS users (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    faculty TEXT NOT NULL,
+    program TEXT NOT NULL
     );
-  `;
+    `;
+  console.log('step second');
+
+  const uniqueUsers = Array.from(
+    new Map(users.map((user) => [user.email, user])).values()
+  );
 
   const insertedUsers = await Promise.all(
-    users.map(async (user) => {
+    uniqueUsers.map(async (user) => {
       const hashedPassword = await bcrypt.hash(user.password, 10);
       return client.sql`
-        INSERT INTO users (id, full_name, email, password, faculty, program)
-        VALUES (${user.id}, ${user.full_name}, ${user.email}, ${hashedPassword}, ${user.faculty}, ${user.program})
-        ON CONFLICT (id) DO NOTHING;
+      INSERT INTO users (id, full_name, email, password, faculty, program)
+      VALUES (${user.id}, ${user.full_name}, ${user.email}, ${hashedPassword}, ${user.faculty}, ${user.program})
+      ON CONFLICT (id) DO NOTHING;
       `;
     })
   );
+  console.log('step third');
 
   return insertedUsers;
 }
@@ -39,6 +45,7 @@ export async function GET() {
 
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
+    console.log({ error });
     await client.sql`ROLLBACK`;
     return Response.json({ error }, { status: 500 });
   }
